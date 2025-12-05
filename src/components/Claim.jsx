77a77b232e1bf419873dd81b5ead6f5d9f542b04
Andreas Haufler <andreas.haufler@durablox.com>
2025-12-05 15:09:43 +0100

    Uses the proper domain in the claim

--- src/components/Claim.jsx
+++ src/components/Claim.jsx
@@ -5,7 +5,7 @@ export default function Claim() {
         <div className="flex gap-1 pr-1">
             <LogoColored className="size-4" />
             <span>Powered by</span>
-            <a className="font-semibold hover:underline" href="https://durablox.com" target="_blank">Durablox.com</a>
+            <a className="font-semibold hover:underline" href="https://durablox.cloud" target="_blank">Durablox.cloud</a>
         </div>
     );
 }
